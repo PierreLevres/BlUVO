f7c43164d07d5e1ba245fa02216339a3450fb1ec
plips-nl <62832923+plips-nl@users.noreply.github.com>
2020-08-10 17:51:42 +0200

    logging procedures changed

--- publish.sh
+++ publish.sh
@@ -1,3 +1,35 @@
-copy files
-if param1 is set then restart domoticz
-else restart the hardware device IN domoticz
+#!/bin/bash
+# attact the directory on the pi
+#mount -t cifs //pi@RPI._smb._tcp.local/pi /Volumes/pi
+
+cd /Volumes/pi/Domoticz/plugins/bluvo/
+
+if [[ plugin.py -ot /Users/peter/Documents/GitHub/bluvo/plugin.py ]];
+then
+  RESTART=True
+else
+  RESTART=False
+fi
+
+RELOAD=False
+FILES=*.py
+
+for f in $FILES
+do
+  # take action on each file. $f store current file name
+  if [[ $f -ot /Users/peter/Documents/GitHub/bluvo/$f ]];
+  then
+    RELOAD=True
+    /bin/cp -rf /Users/peter/Documents/GitHub/bluvo/$f . 2>/dev/null
+  fi
+done
+if [[ "$RESTART" == "True" ]]; then
+  Echo "Restart Domoticz"
+fi
+if [[ "$RELOAD" == "True" ]]; then
+  Echo "Restart Hardware"
+fi
+
+
+
+
